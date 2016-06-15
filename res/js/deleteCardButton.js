0f737c0aef34c4975dbd5f8d1b24c1bf6b17d5ea
William Goodall <wgoodall01@gmail.com>
2016-06-15 18:48:31 -0400

    Fixed modals

--- res/js/deleteCardButton.js
+++ res/js/deleteCardButton.js
@@ -5,33 +5,17 @@ $(document).ready(function(){
         }
         
         var cardId = $(this).data('card-id');
-        
-        BootstrapDialog.show({
-            title:"<h4><b>Confirm Delete Card</b></h4>",
-            message:"Do you really want to delete this card? Deleted cards can't be recovered.",
-            type:BootstrapDialog.TYPE_DANGER,
-            size:BootstrapDialog.SIZE_LARGE,
-            buttons:[
-                {
-                    label: "  Delete",
-                    cssClass:"btn-danger",
-                    icon:"glyphicon glyphicon-trash",
-                    autoSpin:true,
-                    action:function(dialog){
-                        $.post("/remove/" + cardId, function(data){
-                            console.dir(data);
-                            dialog.close();
-                            window.location.href = '/';
-                        })
-                    }
-                },
-                {
-                    label:"Cancel",
-                    action:function(dialog){
-                        dialog.close();
-                    }
-                }
-            ]
-        })
+
+        $(".modal-delete-button").off('click');
+        $(".modal-delete-button").click(function(){
+            $.post("/remove/" + cardId, function(data){
+                console.dir(data);
+                // $(document).emit('beforeunload');
+                $(`[data-card-id=${cardId}]`).closest("tr").remove();
+                $(".delete-modal").modal('hide');
+            })
+        });
+
+        $(".delete-modal").modal();
     });
 });
